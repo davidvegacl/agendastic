1ac43982da7bb1a093b5d8344b839b1ca2e3c81e
David Vega <dvega@prosys.cl>
2020-04-20 01:53:05 -0400

    refactor: Instalador con datos default

--- application/helpers/installation_helper.php
+++ application/helpers/installation_helper.php
@@ -1,4 +1,6 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /* ----------------------------------------------------------------------------
  * Easy!Appointments - Open Source Web Scheduler
@@ -21,11 +23,12 @@
  * Notice: You can add more checks into this file in order to further check the
  * installation state of the application.
  *
- * @return bool Returns whether E!A is installed or not.
+ * @return bool returns whether E!A is installed or not
  */
 function is_ea_installed()
 {
-    $ci =& get_instance();
+    $ci = &get_instance();
+
     return $ci->db->table_exists('ea_users');
 }
 
@@ -37,14 +40,14 @@ function is_ea_installed()
 function get_sample_service()
 {
     return [
-        'name' => 'Test Service',
-        'duration' => 30,
-        'price' => 50.0,
-        'currency' => 'Euro',
-        'description' => 'This is a test service automatically inserted by the installer.',
-        'availabilities_type' => 'flexible',
+        'name' => 'Servicio de pruebas',
+        'duration' => 45,
+        'price' => 10000,
+        'currency' => '$',
+        'description' => 'Este es un servicio de pruebas insertado por el instalador',
+        'availabilities_type' => 'fixed',
         'attendants_number' => 1,
-        'id_service_categories' => NULL
+        'id_service_categories' => null,
     ];
 }
 
@@ -56,21 +59,21 @@ function get_sample_service()
 function get_sample_provider()
 {
     return [
-        'first_name' => 'John',
-        'last_name' => 'Doe',
-        'email' => 'john@doe.com',
+        'first_name' => 'Juan',
+        'last_name' => 'Perez',
+        'email' => 'juan@perez.com',
         'phone_number' => '0123456789',
         'services' => [],
         'settings' => [
-            'username' => 'johndoe',
+            'username' => 'juanperez',
             'password' => '59fe9d073a9c3c606a7e01e402dca4b49b6aa517bd0fdf940c46cb13a7b63dd0',
             'salt' => 'dc5570debc71fc1fe94b1b0aee444fcde5b8cb83d62a6a2b736ead6557d7a2e1',
             'working_plan' => '{"monday":{"start":"09:00","end":"18:00","breaks":[{"start":"14:30","end":"15:00"}]},"tuesday":{"start":"09:00","end":"18:00","breaks":[{"start":"14:30","end":"15:00"}]},"wednesday":{"start":"09:00","end":"18:00","breaks":[{"start":"14:30","end":"15:00"}]},"thursday":{"start":"09:00","end":"18:00","breaks":[{"start":"14:30","end":"15:00"}]},"friday":{"start":"09:00","end":"18:00","breaks":[{"start":"14:30","end":"15:00"}]},"saturday":null,"sunday":null}',
-            'notifications' => FALSE,
-            'google_sync' => FALSE,
+            'notifications' => false,
+            'google_sync' => false,
             'sync_past_days' => 5,
             'sync_future_days' => 5,
-            'calendar_view' => CALENDAR_VIEW_DEFAULT
-        ]
+            'calendar_view' => CALENDAR_VIEW_DEFAULT,
+        ],
     ];
 }
--- application/views/general/installation.php
+++ application/views/general/installation.php
@@ -11,7 +11,7 @@
 
     <style>
         header {
-            background: #DAFFEB;
+            background: #2d3d47;
             margin-bottom: 25px;
         }
 
@@ -61,22 +61,22 @@
 
     <div class="content container-fluid">
         <div class="welcome">
-            <h3>Welcome to the Agendastic installation page.</h3>
+            <h3>Instalación</h3>
         </div>
 
         <div class="alert hidden"></div>
 
         <div class="row">
             <div class="admin-settings col-xs-12 col-sm-5">
-                <h3>Administrator</h3>
+                <h3>Usuario Administrador</h3>
 
                 <div class="form-group">
-                    <label for="first-name" class="control-label">First Name</label>
+                    <label for="first-name" class="control-label">Nombre</label>
                     <input type="text" id="first-name" class="form-control" />
                 </div>
 
                 <div class="form-group">
-                <label for="last-name" class="control-label">Last Name</label>
+                <label for="last-name" class="control-label">Apellido</label>
                 <input type="text" id="last-name" class="form-control" />
                 </div>
 
@@ -86,7 +86,7 @@
                 </div>
 
                 <div class="form-group">
-                    <label for="phone-number" class="control-label">Phone Number</label>
+                    <label for="phone-number" class="control-label">Teléfono</label>
                     <input type="text" id="phone-number" class="form-control" />
                 </div>
 
@@ -101,26 +101,26 @@
                 </div>
 
                 <div class="form-group">
-                    <label for="retype-password" class="control-label">Retype Password</label>
+                    <label for="retype-password" class="control-label">Confirmar Password</label>
                     <input type="password" id="retype-password" class="form-control" />
                 </div>
             </div>
 
             <div class="company-settings col-xs-12 col-sm-5">
-                <h3>Company</h3>
+                <h3>Empresa / Organización</h3>
 
                 <div class="form-group">
-                    <label for="company-name" class="control-label">Company Name</label>
+                    <label for="company-name" class="control-label">Nombre de Empresa / Organización</label>
                     <input type="text" id="company-name" class="form-control" />
                 </div>
 
                 <div class="form-group">
-                    <label for="company-email" class="control-label">Company Email</label>
+                    <label for="company-email" class="control-label">Email Empresa / Organización</label>
                     <input type="text" id="company-email" class="form-control" />
                 </div>
 
                 <div class="form-group">
-                    <label for="company-link" class="control-label">Company Link</label>
+                    <label for="company-link" class="control-label">Sitio Web</label>
                     <input type="text" id="company-link" class="form-control" />
                 </div>
             </div>
@@ -129,19 +129,9 @@
         <br>
 
         <p>
-            You will be able to set your business logic in the backend settings page
-            after the installation is complete.
-            <br>
-            Press the following button to complete the installation process.
-        </p>
-
-        <br>
-
-        <p>
-            <h3>License</h3>
-            Agendastic is licensed under the <span class="label label-default">GPLv3 license</span>.
-            By using the code of Agendastic in any way <br> you agree with the terms described in the
-            following url:
+            <h3>Licencia</h3>
+            Agendastic tiene una licencia <span class="label label-default">GPLv3</span>.
+            Al usar el código de Agendastic en cualquier manera <br> aceptas los términos descritos en la siguiente URL:
             <a href="http://www.gnu.org/copyleft/gpl.html">http://www.gnu.org/copyleft/gpl.html</a>
         </p>
 
@@ -149,7 +139,7 @@
 
         <button type="button" id="install" class="btn btn-success btn-large">
             <i class="icon-white icon-ok"></i>
-            Install Agendastic</button>
+            Comenzar instalación de Agendastic</button>
     </div>
 
     <footer>
